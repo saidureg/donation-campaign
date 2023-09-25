fd747ffabf702c094e16eb675c9380c9bee37d8c
saidureg <saidureg@gmail.com>
2023-09-25 13:16:25 +0600

    each card have 3 different color and change the color dynamically

--- src/components/DonationCard/DonationCard.jsx
+++ src/components/DonationCard/DonationCard.jsx
@@ -1,19 +1,31 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 const DonationCard = ({ donation }) => {
-  const { id, picture, category, title } = donation;
+  const {
+    id,
+    picture,
+    category,
+    title,
+    card_bg_color,
+    text_color,
+    category_bg,
+  } = donation;
   return (
     <div>
       <Link to={`/donation/${id}`}>
-        <div className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-          <div className="relative m-0 overflow-hidden rounded-t-xl bg-transparent bg-clip-border text-gray-700 shadow-none">
+        <div
+          className={`relative flex max-w-[24rem] flex-col rounded-xl bg-[${card_bg_color}] text-[${text_color}] bg-clip-border shadow-md`}
+        >
+          <div className="relative m-0 overflow-hidden rounded-t-xl bg-transparent bg-clip-border shadow-none">
             <img className="w-full" src={picture} alt={title} />
           </div>
           <div className="p-6">
-            <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-              {category}
+            <h4 className="text-sm font-medium leading-snug tracking-normal antialiased">
+              <span className={`bg-[${category_bg}] py-2 px-3 rounded-md`}>
+                {category}
+              </span>
             </h4>
-            <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
+            <p className="mt-3 text-xl font-semibold leading-relaxed antialiased">
               {title}
             </p>
           </div>
