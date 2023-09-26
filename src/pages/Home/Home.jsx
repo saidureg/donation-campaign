ae163192f1a10e0e50d150c5d76fd4d26f9d0255
saidureg <saidureg@gmail.com>
2023-09-26 12:42:35 +0600

    solve search btn responsive related problem

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
         donation.category.toLowerCase() === inputCategoryValue.toLowerCase()
     );
     setFilterCategoryCard(filterDonationCategory);
+    setInputCategoryValue("");
   };
 
   return (
@@ -39,7 +40,7 @@ const Home = () => {
           <div className="max-w-[1320px] mx-auto">
             <div className="hero-content text-center text-neutral-content h-[70vh]">
               <div>
-                <h1 className="mb-8 text-5xl text-[#0B0B0B] font-bold">
+                <h1 className="mb-8 text-2xl md:text-4xl lg:text-5xl text-[#0B0B0B] font-bold">
                   I Grow By Helping People In Need
                 </h1>
                 <div className="relative">
@@ -47,13 +48,13 @@ const Home = () => {
                     onChange={handleSearch}
                     type="text"
                     placeholder="Search here...."
-                    className="input md:w-full max-w-md text-[#0B0B0B66]"
+                    className="input w-11/12 md:w-full max-w-md text-[#0B0B0B66]"
                   />
                   <button
                     onClick={handleSearchBnt}
-                    className="py-3 md:py-3 md:px-5 bg-[#FF444A] rounded-r-lg text-white absolute -ml-[88px] md:-ml-32"
+                    className="py-3 px-5 bg-[#FF444A] rounded-r-lg text-white absolute right-4 md:right-[66px] lg:right-[162px]"
                   >
-                    Get Started
+                    Search
                   </button>
                 </div>
               </div>
